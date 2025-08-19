c0fae7744c865e693d5be3d0b8c4b68329eec3fa
emerss001 <emersonnevess80@gmail.com>
2025-08-18 21:58:34 -0300

    implementando rota para calculo de plano

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,15 @@ import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
 import { env } from "./env.ts";
+import { calcularPlano } from "./routes/calcular-plano.ts";
 
 // Inicializa a aplicação Fastify com suporte ao Zod para validação de tipos
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 // Configura o CORS permitindo acesso do frontend local
 app.register(fastifyCors, {
-    origin: "http://localhost:3001",
-    // origin: "*",
+    // origin: "http://localhost:3001",
+    origin: "*",
 });
 
 // Define os compiladores de validação e serialização usando Zod
@@ -20,6 +21,9 @@ app.setValidatorCompiler(validatorCompiler);
 app.get("/health", async () => {
     return { status: "ok" };
 });
+
+// Registra a rota de cálculo de plano
+app.register(calcularPlano);
 
 // Inicia o servidor
 app.listen({ port: env.PORT }).then(() => {
